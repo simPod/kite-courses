0d088544f36be58625039874f834157e4141ed51
Simon Podlipsky <simon@podlipsky.net>
2025-05-18 11:50:25 +0200

    allow replit

--- vite.config.ts
+++ vite.config.ts
@@ -32,4 +32,7 @@ export default defineConfig({
     outDir: resolve(import.meta.dirname, "dist/public"),
     emptyOutDir: true,
   },
+  server: {
+    allowedHosts: ['localhost', '.replit.dev'],
+  }
 });
